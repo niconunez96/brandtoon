183074b4050ae964d937337901aa621b1e66b79a
niconunez96 <nicolas110996@gmail.com>
2026-04-08 21:30:28 -0300

    enhance skills

---
+++ back/bounded_contexts/shared/diContainer.go
@@ -0,0 +1,14 @@
+package shared
+
+var container *DIContainer
+
+type DIContainer struct {
+}
+
+func NewDIContainer() *DIContainer {
+	if container == nil {
+		container = &DIContainer{}
+	}
+	return container
+}
+
